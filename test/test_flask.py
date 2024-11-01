a237bd474ed12ea78fe428a2d98fcb17e4dfbea1
ashwinchelsea14 <ashwinchelsea14@gmail.com>
2024-11-01 19:02:09 -0400

    added 20 test cases

--- test/test_flask.py
+++ test/test_flask.py
@@ -1,93 +1,217 @@
 from src.app import app, add, mongodb_client
 import pandas as pd
+from werkzeug.datastructures import ImmutableMultiDict
 
 db = mongodb_client.db
 
+client = app.test_client()
 
-def test_home_page():
-    """
-    This test verifies that the home page works correctly
-    """
+def test_invalid_login_empty_ip1():
+    email = ''  # empty email
+    password = ''    # empty password
+    
+    return client.post('/login', data={
+        "email": email,
+        "password": password
+    }, follow_redirects=True)
 
-    response = app.test_client().get('/')
-    assert response.status_code == 200
-    assert b"Welcome to JobCruncher!" in response.data
-    assert b"So why use JobCruncher instead?" in response.data
+def test_invalid_login_empty_ip2():
+    email = ''  # empty email
+    password = 'xyz'    # some random password
+    
+    return client.post('/login', data={
+        "email": email,
+        "password": password
+    }, follow_redirects=True)
 
+def test_invalid_login_empty_ip3():
+    email = 'abc'  # some random email
+    password = ''    # empty password
+    
+    return client.post('/login', data={
+        "email": email,
+        "password": password
+    }, follow_redirects=True)
 
-def test_search_page():
-    """
-    This test verifies that search page displays the user input form correctly
-    """
+def test_invalid_login_empty_ip4():
+    email = ''                     # empty email
+    password = 'correct_password'    # scorrect password password
+    
+    return client.post('/login', data={
+        "email": email,
+        "password": password
+    }, follow_redirects=True)
 
-    response = app.test_client().get('/search')
-    assert response.status_code == 200
-    assert b"Job Title" in response.data
-    assert b"Location" in response.data
-    assert b"Company Name" in response.data
-    assert b"Technical skills" in response.data
-    assert b"Job Type" in response.data
+def test_invalid_login_empty_ip5():
+    email = 'valid@gmail.com'   # valid email
+    password = ''               # empty password
+    
+    return client.post('/login', data={
+        "email": email,
+        "password": password
+    }, follow_redirects=True)
 
+def test_reset_password_ip1():
+    email = ""               # all 3 fields empty
+    New_Password = ""
+    Confirm_password = ""
 
-def test_search_page_submit():
-    """
-    This test verifies that search page filters the data based on the given input
-    """
-
-    add_sample_data()
-    response = app.test_client().post("/search", data={
-        "title": "",
-        "type": "",
-        "location": "",
-        "companyName": "",
-        "skills": "",
+    response = app.test_client().post('/reset', data={
+        "email": email,
+        "new_password": New_Password,
+        "confirm_password": Confirm_password
     })
-    assert response.status_code == 200
 
+def test_reset_password_ip2():
+    email = "valid@email.com"            
+    New_Password = "abc"
+    Confirm_password = ""     # confirm password blank
 
-def test_search_page_submit_zero_results():
-    """
-    This test verifies that the search page works correctly when given input does not match any entries in the database
-    """
-    response = app.test_client().post("/search", data={
-        "title": "zzzzzzzzzzz",
-        "type": "yyyyyyyyyyy",
-        "location": "xxxxxxx",
-        "companyName": "wwwwwwww",
-        "skills": "vvvvvvvvv",
+    response = app.test_client().post('/reset', data={
+        "email": email,
+        "new_password": New_Password,
+        "confirm_password": Confirm_password
     })
-    assert response.status_code == 200
-    assert b"Sorry, there are no current openings with given criteria"
 
+def test_reset_password_ip3():
+    email = "invalid@email.com"    # invalid email        
+    New_Password = "abc"
+    Confirm_password = ""     # confirm password blank
 
-def test_add_db():
-    """
-    This test verifies that sample data can be added to the database successfully
-    """
+    response = app.test_client().post('/reset', data={
+        "email": email,
+        "new_password": New_Password,
+        "confirm_password": Confirm_password
+    })
 
-    add_sample_data()
+def test_reset_password_ip4():
+    email = "valid@email.com"            
+    New_Password = "abc"
+    Confirm_password = "abc"     # all fields are correct
 
+    response = app.test_client().post('/reset', data={
+        "email": email,
+        "new_password": New_Password,
+        "confirm_password": Confirm_password
+    })
 
-def add_sample_data():
-    """
-    This function adds sample data entry to the database
-    """
+def test_reset_password_ip5():
+    email = "valid@email.com"            
+    New_Password = "abc"
+    Confirm_password = "cba"     # both paswords didn't match
 
-    df = pd.DataFrame()
+    response = app.test_client().post('/reset', data={
+        "email": email,
+        "new_password": New_Password,
+        "confirm_password": Confirm_password
+    })
 
-    job_dict = {
-        "Job Title": "Software Engineer, New Grad",
-        "Company Name": "IXL Learning",
-        "Location": "Raleigh, NC",
-        "Date Posted": "2 days ago",
-        "Total Applicants": "Over 200 applicants",
-        "Job Description": "IXL Learning, a leading edtech company with products used by 13 millio…",
-        "Job Link": "https://www.linkedin.com/jobs/view/externalApply/3200864048?url=https%…",
-        "Seniority level": "Not Applicable",
-        "Employment type": "Full-time",
-        "Job function": "Engineering and Information Technology",
-        "Industries": "E-Learning Providers",
-        "skills": "linux,react,ui,scala,sql,python,java"
-    }
-    df = df.append(job_dict, ignore_index=True)
-    add(db, df)
+def test_reset_password_ip6():
+    email = "valid@email"    # valid email without '.com'        
+    New_Password = "abc"
+    Confirm_password = "abc"     
+
+    response = app.test_client().post('/reset', data={
+        "email": email,
+        "new_password": New_Password,
+        "confirm_password": Confirm_password
+    })
+
+def test_reset_password_ip7():
+    email = ""                    # empty email field            
+    New_Password = "abc"
+    Confirm_password = "abc"     # passwords match
+
+    response = app.test_client().post('/reset', data={
+        "email": email,
+        "new_password": New_Password,
+        "confirm_password": Confirm_password
+    })
+
+def test_reset_password_ip8():
+    email = "valid@email.com"                         
+    New_Password = ""                # new password nil
+    Confirm_password = "abc"    
+
+    response = app.test_client().post('/reset', data={
+        "email": email,
+        "new_password": New_Password,
+        "confirm_password": Confirm_password
+    })
+
+def test_reset_password_ip9():
+    email = "valid@email.com"                         
+    New_Password = ""                # new password nil
+    Confirm_password = "abc"    
+
+    response = app.test_client().post('/reset', data={
+        "email": email,
+        "new_password": New_Password,
+        "confirm_password": Confirm_password
+    })
+
+def test_reset_password_ip10():
+    email = "valid@email.com"                         
+    New_Password = ""                
+    Confirm_password = ""    # no passwords 
+
+    response = app.test_client().post('/reset', data={
+        "email": email,
+        "new_password": New_Password,
+        "confirm_password": Confirm_password
+    })
+
+def test_reset_password_ip11():
+    email = "invalid@email.com"    # invalid email        
+    New_Password = "abc"
+    Confirm_password = "abc"     # password's match
+
+    response = app.test_client().post('/reset', data={
+        "email": email,
+        "new_password": New_Password,
+        "confirm_password": Confirm_password
+    })
+
+def test_reset_password_ip12():
+    email = "invalid@email.com"    # invalid email        
+    New_Password = ""         # new password blank
+    Confirm_password = "abc"     
+
+    response = app.test_client().post('/reset', data={
+        "email": email,
+        "new_password": New_Password,
+        "confirm_password": Confirm_password
+    })
+
+def test_reset_password_ip13():
+    email = "invalid@email.com"    # wrong email         
+    New_Password = "abc"
+    Confirm_password = "abc"     # passwords match
+
+    response = app.test_client().post('/reset', data={
+        "email": email,
+        "new_password": New_Password,
+        "confirm_password": Confirm_password
+    })
+
+def test_reset_password_ip14():
+    email = "invalid@email.com"       # wrong email      
+    New_Password = "abc"
+    Confirm_password = "cba"     # both paswords didn't match
+
+    response = app.test_client().post('/reset', data={
+        "email": email,
+        "new_password": New_Password,
+        "confirm_password": Confirm_password
+    })
+
+def test_reset_password_ip15():
+    email = ""       # no email      
+    New_Password = "abc"
+    Confirm_password = "abc"     # paswords match
+
+    response = app.test_client().post('/reset', data={
+        "email": email,
+        "new_password": New_Password,
+        "confirm_password": Confirm_password
+    })
